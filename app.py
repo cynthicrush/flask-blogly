bee22258b7e6999c168f0c4e343f1a718e8e8857
Cynthia Canada <cynthiazcanada@gmail.com>
2021-12-07 16:35:57 -0600

    new code added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request
 # from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug.utils import redirect
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 import pdb
 
 app = Flask(__name__)
@@ -22,22 +22,23 @@ def home_page():
   '''Show the list of all the users in database'''
   return redirect('/users')
 
+# Users route
 
 @app.route('/users')
 def list_users():
   '''Show the list of all the users in database'''
   users = User.query.all()
-  return render_template('list.html', users=users)
+  return render_template('users/list.html', users=users)
 
 
 @app.route('/users/new')
-def show_form():
+def show_add_user_form():
   '''Show the create a user form'''
-  return render_template('form.html')
+  return render_template('users/form.html')
 
 @app.route('/users/new', methods=['POST'])
 def create_user():
-  '''Create a user form'''
+  '''Create a user'''
   first_name = request.form['first-name']
   last_name = request.form['last-name']
   image_url = request.form['image-url']
@@ -53,13 +54,14 @@ def create_user():
 def show_details(user_id):
   '''Show user details'''
   user = User.query.get_or_404(user_id)
-  return render_template('details.html', user=user)
+  # posts = Post.query.get(user_id)
+  return render_template('users/details.html', user=user)#, posts=posts
 
 @app.route('/users/<int:user_id>/edit')
 def show_edit_form(user_id):
   '''Show edit user page'''
   user = User.query.get_or_404(user_id)
-  return render_template('edit.html', user=user)
+  return render_template('users/edit.html', user=user)
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
 def edit_user(user_id):
@@ -80,3 +82,40 @@ def delete_user(user_id):
   db.session.delete(user)
   db.session.commit()
   return redirect('/users')
+
+# Posts route
+
+@app.route('/users/<int:user_id>/posts/new')
+def show_add_post_form(user_id):
+  '''Show the add post form'''
+
+  user = User.query.get_or_404(user_id)
+  return render_template('posts/form.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def create_post(user_id):
+  '''Create a post'''
+  user = User.query.get_or_404(user_id)
+  new_post = Post(title=request.form['title'], 
+                  content=request.form['content'], 
+                  user_id=user.id)
+
+  db.session.add(new_post)
+  db.session.commit()
+  
+  return redirect(f'/users/{user_id}')
+
+@app.route('/posts/<int:post_id>')
+def show_post_details(post_id):
+  '''Show details of a post'''
+
+  post = Post.query.get(post_id)
+
+  return render_template('posts/details.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def show_edit_post_from(post_id):
+  '''Show the form to edit a post'''
+
+  post = Post.query.get(post_id)
+  render_template('posts/edit.html', post=post)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -16,3 +17,12 @@ class User(db.Model):
   first_name = db.Column(db.String(), nullable = False)
   last_name = db.Column(db.String(), nullable = False)
   image_url = db.Column(db.String(), nullable = False, default='https://upload.wikimedia.org/wikipedia/commons/7/7e/Circle-icons-profile.svg')
+
+class Post(db.Model):
+  '''Post'''
+  __tablename__ = 'posts'
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  title = db.Column(db.String(), nullable = False)
+  content = db.Column(db.Text(), nullable = False)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+  user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=False)
