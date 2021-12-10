9e640923630c91408b2f0bb2f20003f3cb122f0e
Cynthia Canada <cynthiazcanada@gmail.com>
2021-12-10 14:41:02 -0600

    code updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request
 # from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug.utils import redirect
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, PostTag, Tag
 import pdb
 
 app = Flask(__name__)
@@ -19,8 +19,8 @@ db.create_all()
 
 @app.route('/')
 def home_page():
-  '''Show the list of all the users in database'''
-  return redirect('/users')
+  '''Show homepage'''
+  return render_template('/homepage.html')
 
 # Users route
 
@@ -39,13 +39,13 @@ def show_add_user_form():
 @app.route('/users/new', methods=['POST'])
 def create_user():
   '''Create a user'''
+  pdb.set_trace()
   first_name = request.form['first-name']
   last_name = request.form['last-name']
   image_url = request.form['image-url']
 
   new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
   db.session.add(new_user)
-  pdb.set_trace()
   db.session.commit()
 
   return redirect('/users')
@@ -58,7 +58,7 @@ def show_details(user_id):
   return render_template('users/details.html', user=user)#, posts=posts
 
 @app.route('/users/<int:user_id>/edit')
-def show_edit_form(user_id):
+def show_edit_user_form(user_id):
   '''Show edit user page'''
   user = User.query.get_or_404(user_id)
   return render_template('users/edit.html', user=user)
@@ -73,7 +73,7 @@ def edit_user(user_id):
 
   db.session.add(user)
   db.session.commit()
-  return redirect('/users')
+  return redirect(f'/users/{user_id}')
 
 @app.route('/users/<int:user_id>/delete', methods=['POST'])
 def delete_user(user_id):
@@ -90,15 +90,19 @@ def show_add_post_form(user_id):
   '''Show the add post form'''
 
   user = User.query.get_or_404(user_id)
-  return render_template('posts/form.html', user=user)
+  tags = Tag.query.all()
+  return render_template('posts/form.html', user=user, tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new', methods=['POST'])
 def create_post(user_id):
   '''Create a post'''
   user = User.query.get_or_404(user_id)
+  tag_ids = [int(num) for num in request.form.getlist('tags')]
+  tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
   new_post = Post(title=request.form['title'], 
                   content=request.form['content'], 
-                  user_id=user.id)
+                  user=user,
+                  tags=tags)
 
   db.session.add(new_post)
   db.session.commit()
@@ -117,8 +121,9 @@ def show_post_details(post_id):
 def show_edit_post_from(post_id):
   '''Show the form to edit a post'''
 
-  post = Post.query.get(post_id)
-  return render_template('posts/edit.html', post=post)
+  post = Post.query.get_or_404(post_id)
+  tags = Tag.query.all()
+  return render_template('posts/edit.html', post=post, tags=tags)
 
 @app.route('/posts/<int:post_id>/edit', methods=['POST'])
 def edit_post(post_id):
@@ -127,6 +132,9 @@ def edit_post(post_id):
   post = Post.query.get_or_404(post_id)
   post.title = request.form['title']
   post.content = request.form['content']
+
+  tag_ids = [int(num) for num in request.form.getlist('tags')]
+  post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
 
   db.session.add(post)
   db.session.commit()
@@ -140,3 +148,58 @@ def delete_post(post_id):
   db.session.delete(post)
   db.session.commit()
   return redirect(f'/users/{post.user_id}')
+
+# Tags route
+
+@app.route('/tags')
+def list_tags():
+  '''List all the tags'''
+  tags = Tag.query.all()
+  return render_template('tags/list.html', tags=tags)
+
+@app.route('/tags/<int:tag_id>')
+def show_tag_details(tag_id):
+  '''Show details of a tag'''
+  tag = Tag.query.get_or_404(tag_id)
+  return render_template('tags/detail.html', tag=tag)
+
+@app.route('/tags/new')
+def show_add_tag_form():
+  '''Show the create a tag form'''
+  return render_template('tags/form.html')
+
+@app.route('/tags/new', methods=['POST'])
+def create_tag():
+  '''Create a tag'''
+  name = request.form['name']
+
+  new_tag = Tag(name=name)
+  db.session.add(new_tag)
+  db.session.commit()
+
+  return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/edit')
+def show_edit_tag_form(tag_id):
+  '''Show edit tag form'''
+  tag = Tag.query.get_or_404(tag_id)
+
+  return render_template('tags/edit.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=['POST'])
+def edit_tag(tag_id):
+  '''Edit a tag'''
+  tag = Tag.query.get_or_404(tag_id)
+  tag.name = request.form['name']
+
+  db.session.add(tag)
+  db.session.commit()
+  return redirect(f'/tags/{tag_id}')
+
+@app.route('/tags/<int:tag_id>/delete', methods=['POST'])
+def delete_tag(tag_id):
+  '''Delete a tag'''
+  tag = Tag.query.get_or_404(tag_id)
+  db.session.delete(tag)
+  db.session.commit()
+  return redirect('/tags')
--- models.py
+++ models.py
@@ -14,9 +14,9 @@ class User(db.Model):
   '''User'''
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-  first_name = db.Column(db.String(), nullable = False)
-  last_name = db.Column(db.String(), nullable = False)
-  image_url = db.Column(db.String(), nullable = False, default='https://upload.wikimedia.org/wikipedia/commons/7/7e/Circle-icons-profile.svg')
+  first_name = db.Column(db.String, nullable = False)
+  last_name = db.Column(db.String, nullable = False)
+  image_url = db.Column(db.String, nullable = False, default='https://upload.wikimedia.org/wikipedia/commons/7/7e/Circle-icons-profile.svg')
 
   posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
 
@@ -24,7 +24,29 @@ class Post(db.Model):
   '''Post'''
   __tablename__ = 'posts'
   id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-  title = db.Column(db.String(), nullable = False)
-  content = db.Column(db.Text(), nullable = False)
+  title = db.Column(db.String, nullable = False)
+  content = db.Column(db.Text, nullable = False)
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-  user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+  @property
+  def friendly_date(self):
+      """Return nicely-formatted date."""
+
+      return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+class PostTag(db.Model):
+  '''Post tag'''
+  __tablename__ = 'post_tags'
+  post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False, primary_key=True)
+  tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False, primary_key=True)
+
+class Tag(db.Model):
+  '''Tag'''
+  __tablename__='tags'
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  name = db.Column(db.Text, nullable=False, unique=True)
+
+  posts = db.relationship("Post",secondary='post_tags', backref="tags")#, cascade="all, delete-orphan")
+
